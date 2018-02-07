97bc803af673aaff4c453c271418e98e39216707
darioad7 <darioadpg@gmail.com>
2018-02-07 17:19:03 +0100

    Vistazo general
    Corregidas tildes para evitar problemas.
    Tarea: Añadir el campo tipo en los html.

--- src/main/java/asw/Application.java
+++ src/main/java/asw/Application.java
@@ -27,31 +27,31 @@ public class Application {
 
 		return (args) -> {
 
-			// Inserción en la base de datos
-			repository.save(new Agent("Paco Gomez", "123456", "paco@hotmail.com", "12345678B", "Calle Uría", "Person", 1));
+			// Insercion en la base de datos
+			repository.save(new Agent("Paco Gomez", "123456", "paco@hotmail.com", "12345678B", "Calle Uria", "Person", 1));
 
-			// Inserción en la base de datos
-			repository.save(new Agent("Pepe Fernández", "123456", "pepe@gmail.com", "87654321B", "Calle Principal", "Person", 1));
+			// Insercion en la base de datos
+			repository.save(new Agent("Pepe Fernandez", "123456", "pepe@gmail.com", "87654321B", "Calle Principal", "Person", 1));
 		
 
-			// Inserción en la base de datos
-			repository.save(new Agent("Carmen López", "123456", "carmen@yahoo.com", "11223344C", "Calle Calvo Sotelo", "Person", 1));
+			// Insercion en la base de datos
+			repository.save(new Agent("Carmen Lopez", "123456", "carmen@yahoo.com", "11223344C", "Calle Calvo Sotelo", "Person", 1));
 
 
 
-			// Inserción en la base de datos
-			repository.save(new Agent("Isabel Rodríguez", "123456", "isabel@gmail.com", "22334455D", "Avenida Galicia", "Person", 1));
+			// Insercion en la base de datos
+			repository.save(new Agent("Isabel Rodriguez", "123456", "isabel@gmail.com", "22334455D", "Avenida Galicia", "Person", 1));
 
 
 
 
-			// Inserción en la base de datos
-			repository.save(new Agent("María Sánchez", "123456", "maria@gmai.com", "33445566E", "Avenida Santander", "Person", 1));
+			// Insercion en la base de datos
+			repository.save(new Agent("Marta Sanchez", "123456", "maria@gmai.com", "33445566E", "Avenida Santander", "Person", 1));
 
 		
 
 
-			// Inserción en la base de datos
+			// Insercion en la base de datos
 			repository.save(new Agent("Jose Ballesteros", "123456", "jose@gmail.com", "44556677F", "Calle Asturias", "Person", 1));
 
 		
